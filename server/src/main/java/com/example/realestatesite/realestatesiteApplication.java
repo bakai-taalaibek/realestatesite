66e991f14203e7ffbdfc5c84032f2cdffb8329ff
Admin <abdutokt2004>
2023-05-08 16:01:57 +0600

    Добавил maven в проект а также модуль Spring boot. Из зависимостей добавил: web, lombok, jpa, security, swagger, mapper. Проверил всё запускается, со стороны бэкенда кто будет запускать измените файл applicartion.properties

---
+++ server/src/main/java/com/example/realestatesite/realestatesiteApplication.java
@@ -0,0 +1,12 @@
+package com.example.realestatesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class realestatesiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(realestatesiteApplication.class, args);
+    }
+}
