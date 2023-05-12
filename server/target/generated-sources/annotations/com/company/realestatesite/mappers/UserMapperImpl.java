bb776b668f277c34381a655fa362284eb131ede0
Kulonbekov <kulonbekov@gmail.com>
2023-05-12 21:22:27 +0600

    слияние

---
+++ server/target/generated-sources/annotations/com/company/realestatesite/mappers/UserMapperImpl.java
@@ -0,0 +1,65 @@
+package com.company.realestatesite.mappers;
+
+import com.company.realestatesite.models.dtos.UserDto;
+import com.company.realestatesite.models.entities.User;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-05-12T20:31:10+0600",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.18 (Eclipse Adoptium)"
+)
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public User toEntity(UserDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        return user;
+    }
+
+    @Override
+    public UserDto toDto(User entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        UserDto userDto = new UserDto();
+
+        return userDto;
+    }
+
+    @Override
+    public List<User> toEntities(List<UserDto> dtoList) {
+        if ( dtoList == null ) {
+            return null;
+        }
+
+        List<User> list = new ArrayList<User>( dtoList.size() );
+        for ( UserDto userDto : dtoList ) {
+            list.add( toEntity( userDto ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<UserDto> toDtos(List<User> entityList) {
+        if ( entityList == null ) {
+            return null;
+        }
+
+        List<UserDto> list = new ArrayList<UserDto>( entityList.size() );
+        for ( User user : entityList ) {
+            list.add( toDto( user ) );
+        }
+
+        return list;
+    }
+}
