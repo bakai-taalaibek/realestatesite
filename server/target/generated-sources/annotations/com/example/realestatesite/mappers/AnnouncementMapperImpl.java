952ccf65cb4c73374b3df3723b9ad49dae044afa
Admin <abdutokt2004>
2023-05-12 16:16:20 +0600

    При работе обратите внимание на application.properties.

--- server/target/generated-sources/annotations/com/example/realestatesite/mappers/AnnouncementMapperImpl.java
+++ server/target/generated-sources/annotations/com/example/realestatesite/mappers/AnnouncementMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-12T15:19:52+0600",
+    date = "2023-05-12T16:14:14+0600",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 18.0.2 (Oracle Corporation)"
 )
 public class AnnouncementMapperImpl implements AnnouncementMapper {
