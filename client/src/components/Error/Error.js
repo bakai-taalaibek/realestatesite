08809264ae004b710978addc07b6ce489ea84978
eliza askarova <askarovaeliza01@gmail.com>
2023-05-19 20:12:10 +0600

    error message

--- client/src/components/Error/Error.js
+++ client/src/components/Error/Error.js
@@ -1,7 +1,23 @@
-import React from "react";
+import { useRouteError } from "react-router-dom";
+import Errorr from "./aseets/error.png";
 
-const Error = () => {
-  return <div></div>;
-};
+export default function Error() {
+  const error = useRouteError();
 
-export default Error;
+  return (
+    <div className={errorMessageElement}>
+      <i className="text-[#828282]">{error.statusText || error.message}</i>
+      <h1 className="text-4xl text-[40px] font-bold text-black-400">Ошибка</h1>
+      <p className="text-[24px] font-bold text-[#828282]">
+        Попробуйте перезагрузить страницу
+      </p>
+      <img src={Errorr} alt="" />
+      <p className="text-gray-500"></p>
+    </div>
+  );
+}
+
+const errorMessageElement = ` 
+  min-h-screen grid content-center justify-items-center 
+  [&>*]:m-2 pb-10 
+  text-xl `;
